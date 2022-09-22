d2cc0345007038821eae3d303851cd1308071876
elenaNikolaenya <rushka@list.ru>
2022-09-23 00:12:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ cardContainer.addEventListener('click', (event) => {
         itemInCart['size'] = size.textContent;
         //добавим купленный предмет (объект) в корзину (в массив)
         itemsInCart.push(itemInCart);
-        sessionStorage.setItem('cart', itemsInCart);        
+        localStorage.setItem('cart', itemsInCart);        
       }
     }
 
